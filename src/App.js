3382b389c20b8abb66590b3f41a0614f2b7a8744
Divyanshi1598 <15tiwaridivyanshi@gmail.com>
2022-01-01 17:26:10 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+ import Navbar from './components/Navbar';
+import Home from "./components/pages/Home";
+import Body from "./components/pages/Body";
+import Hair from "./components/pages/Hair";
+import Cart from "./components/pages/Cart";
+import Fragrance  from "./components/pages/Fragrance";
+import Gifts from "./components/pages/Gifts";
+import Kits from "./components/pages/Kits";
+import Read from "./components/pages/Read";
+import SkinCare from "./components/pages/SkinCare";
+import Stores from "./components/pages/Stores";
+import Login from "./components/pages/Login";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <Router>
+     
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/SkinCare" element={<SkinCare />}/>
+        <Route path="/Body" element={<Body />}/>
+        <Route path="/Fragrance" element={<Fragrance />}/>
+        <Route path="/Hair" element={<Hair />}/>
+        <Route path="/Kits" element={<Kits />}/>
+        <Route path="/Read" element={<Read />}/>
+        <Route path="/Gifts" element={<Gifts />}/>
+      </Routes>
+    </Router>
+   
   );
 }
 
