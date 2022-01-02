ecf5c796dd83e444c38446d31f09cfa92ba251d6
Divyanshi1598 <15tiwaridivyanshi@gmail.com>
2022-01-02 12:05:48 +0530

    Added HashRouter in app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom'
 
 import App from './App';
 //import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
